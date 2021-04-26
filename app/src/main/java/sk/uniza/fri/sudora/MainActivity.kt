4ff0379abe3af42279dc89d99ef307228b787564
Filipson <filip.sudora@gmail.com>
2021-04-26 20:23:52 +0200

    Some features

--- app/src/main/java/sk/uniza/fri/sudora/MainActivity.kt
+++ app/src/main/java/sk/uniza/fri/sudora/MainActivity.kt
@@ -2,10 +2,21 @@ package sk.uniza.fri.sudora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val newNoteFab: View = findViewById(R.id.new_note_button)
+        newNoteFab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+
     }
 }
